9c7d06924c78226e8a3f67888e2778ef2ebee892
Petrick Jerico <petrickjerico@gmail.com>
2024-07-24 19:07:11 +0700

    add hover effect for group name chip

--- src/client/components/DisplayGroupBox.tsx
+++ src/client/components/DisplayGroupBox.tsx
@@ -1,5 +1,6 @@
 import { Button, Chip, Stack, useColorScheme } from '@mui/joy'
 import { getGroupName } from '../utils/titles'
+import { useState } from 'react'
 
 export default function DisplayGroupBox({
   groupId,
@@ -12,15 +13,21 @@ export default function DisplayGroupBox({
   isDisabled?: boolean,
   namePlacement?: 'top' | 'bottom'
 }) {
-
   const { mode } = useColorScheme()
   const isVisible = !isDisabled && !!namePlacement
   const groupName = getGroupName(groupId.slice(-1))
+  const [hover, setHover] = useState<boolean>(false)
 
   return (
     <Stack gap={2} alignItems='center'>
-      {namePlacement === 'top' && (<Chip size='lg' sx={{ visibility: isVisible ? undefined : 'hidden' }}>{groupName}</Chip>)}
-      <Button variant='soft' onClick={onClick} size='sm' disabled={isDisabled}>
+      {namePlacement === 'top' && <GroupNameChip isVisible={isVisible} isHover={hover} name={groupName} />}
+      <Button
+        variant='soft'
+        onClick={onClick}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
+        disabled={isDisabled}
+      >
         <img
           src={require(`../../assets/img/${groupId}.png`)}
           alt={groupId}
@@ -29,7 +36,21 @@ export default function DisplayGroupBox({
           className={mode === 'dark' ? 'invert' : undefined}
         />
       </Button >
-      {namePlacement === 'bottom' && (<Chip size='lg' sx={{ visibility: isVisible ? undefined : 'hidden' }}>{groupName}</Chip>)}
+      {namePlacement === 'bottom' && <GroupNameChip isVisible={isVisible} isHover={hover} name={groupName} />}
     </Stack>
+  )
+}
+
+function GroupNameChip({ name, isVisible, isHover }: { name: string, isVisible: boolean, isHover: boolean }) {
+  return (
+    <Chip
+      size='lg'
+      sx={(theme) => ({
+        visibility: isVisible ? undefined : 'hidden',
+        backgroundColor: isHover ? theme.vars.palette.neutral.softHoverBg : theme.vars.palette.neutral.softBg,
+        opacity: isHover ? '100%' : '80%'
+      })}>
+      {name}
+    </Chip>
   )
 }
